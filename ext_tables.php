1f68029d52cb72b9bef9e73eade7f50abe1afb68
Johannes Hertenstein <johannes@nimius.net>
2017-05-09 16:39:38 +0200

    TASK | Change Label of TypoScript include

--- ext_tables.php
+++ ext_tables.php
@@ -20,7 +20,7 @@
 );
 
 // Add static extension TypoScript template.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Default TypoScript');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Location Manager');
 
 // Register datamap processing.
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['EXT:NIMIUS.' . $_EXTKEY] = 'NIMIUS\\LocationManager\\Hook\\TceMainDataMapperHook';
